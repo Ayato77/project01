d4670b84cfed651766b156ba26078061f20f9dc8
Otoya <otoya.nakakaze@gmail.com>
2020-03-15 18:49:27 +0100

    Setup admin site

--- django_app/to_do/admin.py
+++ django_app/to_do/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
-# Register your models here.
+# クラスのインポート
+from .models import Todo
+
+# adminサイトのセットアップ
+# Djangoのドキュメントを読むことを推奨
+# passは何もしない命令
+
+@admin.register(Todo)
+class Todo(admin.ModelAdmin):
+    pass
