5d719332e1fdcbe91482932f5d3d1b466d08a1d2
Otoya <otoya.nakakaze@gmail.com>
2020-03-21 19:37:17 +0100

    Update todo.service.ts

--- ng2app/src/app/services/todo.service.ts
+++ ng2app/src/app/services/todo.service.ts
@@ -8,17 +8,29 @@ import { Todo } from '../models/todo.model';
 @Injectable()
 export class TodoService {
   todo: Todo[] = [];
-  private Url = `http://127.0.0.1:8000/api/todo/`
+  private httpOptions: any = {
+    // ヘッダ情報
+    headers: new HttpHeaders({
+      'Content-Type': 'application/json'
+    }),
+    // DELETE 実行時に `body` が必要になるケースがあるのでプロパティとして用意しておく
+    // ( ここで用意しなくても追加できるけど... )
+    body: null
+  };
+
+  private host: string = 'http://127.0.0.1:8000/api/todo';
   private headers = new HttpHeaders({'Content-Type': 'application/json'});
 
   constructor(
     private http: HttpClient
-  ) { }
+  ) { // `Authorization` に `Bearer トークン` をセットする
+    this.setAuthorization('my-auth-token');
+  }
 
   // 全てのtodoをGETする
   getAllTodo(): Promise<Todo[]> {
     return this.http
-      .get(this.Url)
+      .get(this.host, this.httpOptions)
       .toPromise()
       .then((res) => {
       // response の型は any ではなく class で型を定義した方が良いが
@@ -36,4 +48,23 @@ export class TodoService {
     console.log('Error occured.', err);
     return Promise.reject(err.message || err);
   }
+
+  /**
+   * Authorizatino に認証トークンを設定しする
+   *
+   * @param {string} token 認証トークン
+   * @returns {void}
+   * @memberof HttpClientService
+   * @description
+   * トークンを動的に設定できるようメソッド化している
+   * Bearer トークンをヘッダに設定したい場合はこのメソッドを利用する
+   */
+  public setAuthorization(token: string = null): void {
+    if (!token) {
+      return;
+    }
+    const bearerToken: string = `Bearer ${token}`;
+    this.httpOptions.headers = this.httpOptions.headers.set('Authorization', bearerToken);
+  }
+
 }
