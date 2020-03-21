4f6d3a440fce811d292bccfc051c5d12400cd32b
Otoya <otoya.nakakaze@gmail.com>
2020-03-21 16:19:42 +0100

    Update app-routing.module.ts
    TodoListComponentをアプリのスタートページに表示されるようにルーティング

--- ng2app/src/app/app-routing.module.ts
+++ ng2app/src/app/app-routing.module.ts
@@ -1,8 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+import {TodoListComponent} from './components/todo-list.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  //TodoListComponentへ向けてルーティング
+  {path:'', component: TodoListComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
